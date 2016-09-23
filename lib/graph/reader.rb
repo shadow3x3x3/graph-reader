4a6346875b3dfcae5ed1e4d0e841ec8ceed251d7
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-09-23 12:28:12 +0800

    init commit

---
+++ lib/graph/reader.rb
@@ -0,0 +1,7 @@
+require "graph/reader/version"
+
+module Graph
+  module Reader
+    # Your code goes here...
+  end
+end
---
+++ lib/graph/reader/version.rb
@@ -0,0 +1,5 @@
+module Graph
+  module Reader
+    VERSION = "0.1.0"
+  end
+end
